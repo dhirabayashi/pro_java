29d186f2ecbd1ba90e9b106636b4bb6dec1bddc7
daiki <gigabowser@hotmail.co.jp>
2022-06-02 20:10:31 +0900

    ソケットサンプルの追加

---
+++ src/main/java/projava/SimpleServer.java
@@ -0,0 +1,20 @@
+package projava;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleServer {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Waiting...");
+
+        try(var server = new ServerSocket(1700);
+            Socket soc = server.accept();
+            InputStream input = soc.getInputStream()
+        ) {
+            System.out.println("connect from " + soc.getInetAddress());
+            System.out.println(input.read());
+        }
+    }
+}
